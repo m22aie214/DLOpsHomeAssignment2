2925834d6948376f2197bb2cbf5eb188ad1dc004
m22aie214 <m22aie214@iitj.ac.in>
2024-03-30 13:08:11 +0530

    bug values not printing

--- sigmoidValuesActivity1.py
+++ sigmoidValuesActivity1.py
@@ -7,7 +7,4 @@ def sigmoid(x):
 # Given data
 random_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
 
-# Calculate and print sigmoid values for each data point
-for value in random_values:
-    sigmoid_value = sigmoid(value)
-    print(f"Sigmoid({value}) = {sigmoid_value}")
+
